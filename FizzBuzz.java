429015162ac81ebad6d2555400261d769d7ee59b
qi203 <qi203@nyu.edu>
2017-03-05 06:38:59 -0500

    Fizz Buzz Test Exercie

---
+++ FizzBuzz.java
@@ -0,0 +1,13 @@
+public class FizzBuzz {
+
+public class FizzBuzz {
+	public String fizz(Integer inputNumber){
+		if ((inputNumber % 3 == 0) && (inputNumber % 5 == 0))
+			return "fizzbuzz";
+		else if (inputNumber % 3 == 0)
+			return "fizz";
+		else if (inputNumber % 5 == 0)
+			return "buzz";
+		return inputNumber.toString();
+	}
+}
---
+++ FizzBuzzTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+import static junit.framework.TestCase.assertEquals;
+
+public class FizzBuzzTest{
+	
+	FizzBuzz fizzBuzz;
+	
+	@Before
+	public void setUp() {
+		fizzBuzz = new FizzBuzz();
+	}
+	@Test
+	public void whenFizzBuzzIsPassedANumberItReturnsThatNumber(){
+		assertEquals("1", fizzBuzz.fizz(1));
+	}
+
+	@Test
+	public void whenFizzBuzzIsPassedATwoItReturnsATwo(){
+		assertEquals("2", fizzBuzz.fizz(2));
+	}
+
+	@Test
+	public void whenFizzBuzzIsPassedAMultipleOfThreeItReturnsFizz(){
+		assertEquals("fizz", fizzBuzz.fizz(3));
+		assertEquals("fizz", fizzBuzz.fizz(6));
+	}
+
+	@Test
+	public void whenFizzBuzzIsPassedAMultipleOfFiveItReturnsBuzz(){
+		assertEquals("buzz", fizzBuzz.fizz(5));
+		assertEquals("buzz", fizzBuzz.fizz(10));
+	}
+	@Test
+	public void whenFizzBuzzIsPassedAMultipleOfThreeAndFiveItReturnsFizzBuzz(){
+		assertEquals("fizzbuzz", fizzBuzz.fizz(15));
+	}
+}
